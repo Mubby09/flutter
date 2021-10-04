173d8a39ce35ff71a98d34aa14b74a752f407d23
Mubarak Akinsola <akinsolamubarak1996@gmail.com>
2021-10-04 20:20:57 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart'; 
+import '../route.dart';
 
 void main() => runApp(MyApp());
 
+
+
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: RandomWords(), 
-        ),
-      ),
+      title: 'Startup Name Generator',
+      theme: ThemeData(          
+        primaryColor: Colors.white,
+      ),     
+      home: RandomWords(),
     );
   }
 }
@@ -27,12 +26,138 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
-
-  final _suggestions = <WordPair>[];                
+  final _suggestions = <WordPair>[];    
+  final _saved = <WordPair>{};             
   final _biggerFont = const TextStyle(fontSize: 18);
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();    
-    return Text(wordPair.asPascalCase);  
+    return Scaffold (                   
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+         actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    ); 
+  }
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(  
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+    ),  
+    onTap: () {      
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else { 
+          _saved.add(pair); 
+        } 
+      });
+    },           
+    );
+  }
+
+   void _pushSaved() {
+      Navigator.of(context).push(
+         MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(context: context, tiles: tiles).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ), 
+  );
+  }
+
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+   {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Open route'),
+          onPressed: () {
+           onPressed: () {
+           Navigator.push(
+           context,
+           MaterialPageRoute(builder: (context) => SecondRoute()),
+          );
+          };
+          },
+        ),
+      ),
+    );
   }
 }
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            onPressed: () {
+            Navigator.pop(context);
+            };
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
